ce7900814575eed69ba1e21557fceb7c9f9eae95
Bente <Bente@LAPTOP-5K2FGGCN>
2017-12-10 11:51:32 +0100

    Added javadoc

--- SemesterProjectPhaseTwo/src/Game/CraftableItem.java
+++ SemesterProjectPhaseTwo/src/Game/CraftableItem.java
@@ -6,41 +6,31 @@
 package Game;
 
 import java.util.ArrayList;
- 
+
 /**
  *
  * @author Nicolai
  */
 public class CraftableItem extends Item {
 
-    //HashMap used for making a itemlist of each room
-//    static HashMap<String, Item> craftableList = new HashMap<>();
+    /**
+     * ArrayList used for storing craftableItems
+     */
     static ArrayList<Item> craftableListArray = new ArrayList<>();
-//    private String name;
-//    static Item campfire = new CraftableItem("Campfire", "Campfire: Lumber, Stick and Flint", 3);
-//    static Item spear = new CraftableItem("Spear", "Spear: Stick, Fint and Rope or Lian", 3);
-//    static Item axe = new CraftableItem("Axe", "Axe: Stick, Stone and Rope or Lian", 3);
-//    static Item raft = new CraftableItem("Raft", "Raft: Lumber, Stick and Rope or Lian", 3);
-
+    
+    /**
+     * No-args contructor
+     */
     public CraftableItem() {
-
     }
 
+    /**
+     * 
+     * @param name is the name for which the item is represented
+     * @param description stores the requirements when crafting
+     * @param weight is the item weight
+     */
     public CraftableItem(String name, String description, int weight) {
         super(name, description, weight, false);
     }
-
-//    @Override
-//    public String toString() {
-//        String craftableItems = "";
-//        Iterator iteratorCraftableItem = craftableList.entrySet().iterator();
-//
-//        
-//            HashMap.Entry entry = (HashMap.Entry) iteratorCraftableItem.next();
-//            craftableItems += (String) entry.getKey();
-//            craftableItems += "\n";
-//
-//        return craftableItems;
-//    }
-   
 }
